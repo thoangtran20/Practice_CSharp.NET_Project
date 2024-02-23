50b1a82be3e1436cb89576a69622cafccfce7f32
thoangtran20 <thoangtran20@gmail.com>
2024-02-23 18:33:23 +0700

    Set up simple project ASP.NET Core Blazor Server TodoList Application

---
+++ TodoListApp_Blazor/TodoListApp_Blazor/Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApp_Blazor.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ TodoListApp_Blazor/TodoListApp_Blazor/Data/TodoItemService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace TodoListApp_Blazor.Data
+{
+    public class TodoItemService
+    {
+        private static List<TodoItem> _data = new List<TodoItem>()
+        {
+           new TodoItem {Title = "Learn C# ASP.NET Core MVC" },
+           new TodoItem {Title = "Learn C# ASP.NET Core Razor page" },
+           new TodoItem {Title = "Learn C# ASP.NET Core Blazor" },
+           new TodoItem {Title = "Learn C# ASP.NET Core Web API" },
+           new TodoItem {Title = "Build a Project C# ASP.CORE" },
+        };
+        private readonly string _file = "Data\\todo.json";
+        public List<TodoItem> GetData()
+        {
+            if (File.Exists(_file)) 
+            {
+                using var file = File.OpenText(_file);
+                var serializer = new JsonSerializer();
+                _data = serializer.Deserialize(file, typeof(List<TodoItem>)) as List<TodoItem>;
+            }
+            return _data;
+        }
+        public void SaveChanges()
+        {
+            using var file = File.CreateText(_file);
+            var serializer = new JsonSerializer();
+            serializer.Serialize(file, _data);
+        }
+    }
+}
