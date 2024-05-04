2c2caadbfce91d097d9632e9e7241a99dec683d2
thoangtran20 <thoangtran20@gmail.com>
2024-05-04 11:34:44 +0700

    Update code - Add some Razor Files to Edit City, Delete City and Display Employee List, Create New Employee, Edit Employee and Delete Employee - Handle validate when create and edit empty data

--- Blazor/BlazorDapperSPA/BlazorDapperSPA/Models/Employee.cs
+++ Blazor/BlazorDapperSPA/BlazorDapperSPA/Models/Employee.cs
@@ -1,11 +1,17 @@
-﻿namespace BlazorDapperSPA.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDapperSPA.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Department is required")]
         public string Department { get; set; }
+        [Required(ErrorMessage = "Designation is required")]
         public string Designation { get; set; }
+        [Required(ErrorMessage = "Company is required")]
         public string Company { get; set; }
         public string CityId { get; set; }
 
